993283d47ccd90e3c0d12fa71968eb9e1672a692
4nteros <bogatovandrej345@gmail.com>
2024-10-11 18:59:58 +0200

    Answered 2 questions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 Ответы на вопросы:
 
 1) Переменная - это особым образом обозначенная часть памяти
+2) Типы данных:
+
+Целочисленные: int, short, long, long long
+Целочисленные без знака: unsigned int, unsigned short, unsigned long, unsigned long long
+Вещественные: float, double, long double
+Символьные: char, unsigned char
+Логический - bool
 
 
 */
